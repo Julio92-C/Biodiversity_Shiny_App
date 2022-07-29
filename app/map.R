f07809d97896460e610da22b9850fb78c2e67acb
Julio92-C <juliocesar921016@gmail.com>
2022-07-29 09:09:42 +0800

    Add map.R file

--- app/map.R
+++ app/map.R
@@ -1,16 +1,23 @@
 
-
+library(dplyr)
 library(readr)
 library(leaflet)
 
+# Load the dataset
 poland <- read_csv("data/poland.csv")
+head(poland)
+colnames(poland)
 
-View(poland)
+# Set up some variable
+scientificName <- unique(poland$scientificName)
+locality <- as.factor(poland$locality)
 
+# Filter the dataset by scientificName == Alces alces and locality == Warszawa
+alcesAlces <- dplyr::filter(poland, poland$scientificName == "Alces alces" & poland$locality == "Poland - Podlaskie")
 
 m <- leaflet()
 m <- addTiles(m)
-m <- addMarkers(m, lng=174.768, lat=-36.852, popup="The birthplace of R")
+m <- addMarkers(m, lng=poland$longitudeDecimal, lat=poland$latitudeDecimal, popup=poland$scientificName)
 m
 
 # CLEAN UP #################################################
